b090f685472f85f9003cb63f853051ffea90ac73
NastyaNed <acherkaeva1997@gmail.com>
2022-11-17 19:58:35 +0300

    added Task2

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -6,3 +6,25 @@
 
 // 32679 -> 6
 
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int ToThirdDigit(int number)
+{
+    while (number > 1000)
+    {
+        number = number / 10;
+    }
+    return number % 10;
+}
+
+if(number<0) number=-number;
+if (number > 99)
+{
+    int result = ToThirdDigit(number);
+    Console.WriteLine(result);
+}
+else 
+{
+    Console.WriteLine("Третьей цифры нет");
+}
